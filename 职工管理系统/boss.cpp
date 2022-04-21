1b06bc103d1ae93a0bf8d16ae35f95c335525986
S2O3 <60221655+S2O3@users.noreply.github.com>
2022-04-21 23:15:37 +0800

    Add files via upload

---
+++ 职工管理系统/boss.cpp
@@ -0,0 +1,18 @@
+#include"boss.h"
+Boss::Boss(int id, string name, int did)
+{
+	this->m_ID = id;
+	this->m_Name = name;
+	this->m_DeptID = did;
+}
+void Boss::showInfo()
+{
+	cout << "职工编号：" << this->m_ID
+		<< "\t职工姓名：" << this->m_Name
+		<< "\t岗位：" << this->getDeptName()
+		<< "\t岗位职责：分发任务" << endl;
+}
+string Boss::getDeptName()
+{
+	return string("老板");
+}
